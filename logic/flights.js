0e30935c9fd53470c228c5d784095b9894812b74
Sara Pezzotta <sarapezzotta20@hotmail.it>
2023-02-04 14:54:43 +1100

    first commit

--- logic/flights.js
+++ logic/flights.js
@@ -1,4 +1,17 @@
 function Flights() {
+    function calculateNumberOfFlights(numberOfPassengers, flightCapacity){ 
+        if (numberOfPassengers % flightCapacity === 0);
+        return numberOfPassengers/flightCapacity;
 
+        if (numberOfPassengers < 0) console.log("The number of passengers must be a positive integer value");
+        if (flightCapacity < 0) console.log("The capacity of the flight must be a positive integer value");    
+
+        }
+
+        
+    }
+    return {calculateNumberOfFlights};
 }
+
+module.exports = Flights();
 
