ce5b547296c5523390fbcc98ec473da0d84a3fb2
Sara Pezzotta <sarapezzotta20@hotmail.it>
2023-02-11 11:49:43 +1100

    4

--- logic/flights.js
+++ logic/flights.js
@@ -1,44 +1,51 @@
 function Flights() {
-    function calculateNumberOfFlights(passengers, capacity) {
-        let flights;
+  function calculateNumberOfFlights(passengers, capacity) {
+    let flights;
 
-        if ((passengers < 0) || (!Number.isInteger(Number(passengers)))) {
-            throw new Error("The number of passengers must be a positive integer value")
-        }
+    if (passengers < 0 || !Number.isInteger(Number(passengers))) {
+      throw new Error(
+        "The number of passengers must be a positive integer value"
+      );
+    }
 
-        if ((capacity < 0) || (!Number.isInteger(Number(capacity)))) {
-            throw new Error("The capacity of the flight must be a positive integer value")
-        }
+    if (capacity < 0 || !Number.isInteger(Number(capacity))) {
+      throw new Error(
+        "The capacity of the flight must be a positive integer value"
+      );
+    }
 
-        if (passengers % capacity == 0) {
-            flights = passengers/capacity;
-        } else {
-            flights = Math.floor(passengers/capacity) + 1;
-        }
-        return flights;
-    } 
+    if (passengers % capacity == 0) {
+      flights = passengers / capacity;
+    } else {
+      flights = Math.floor(passengers / capacity) + 1;
+    }
+    return flights;
+  }
 
   function checkAircraftRevision(distanceLimit, distancesArray) {
     let totalDistance = 0;
     let distance;
-    for(distance of distancesArray) {
-        totalDistance += distance;
+    for (distance of distancesArray) {
+      totalDistance += distance; // calculates the total distance as the sum of the distances in the array
     }
     if (totalDistance > distanceLimit) {
-        throw new Error("Flight maximum allowed distance (" + distanceLimit + ") exceeded. No flight is allowed any longer, you need to make the revision immediately.");
+      throw new Error(
+        "Flight maximum allowed distance (" +
+          distanceLimit +
+          ") exceeded. No flight is allowed any longer, you need to make the revision immediately."
+      );
     }
 
-    if (totalDistance <= distanceLimit/2) {
-        return "The revision needs to be done within the next 3 months";
-    } else if (totalDistance <= 3 * distanceLimit/4) {
-        return "The revision needs to be done within the next 2 months";
+    if (totalDistance <= distanceLimit / 2) {
+      return "The revision needs to be done within the next 3 months";
+    } else if (totalDistance <= (3 * distanceLimit) / 4) {
+      return "The revision needs to be done within the next 2 months";
     } else {
-        return "The revision needs to be done within the next month";
+      return "The revision needs to be done within the next month";
     }
- }
+  }
 
-    
-    return { calculateNumberOfFlights, checkAircraftRevision };
-
+  return { calculateNumberOfFlights, checkAircraftRevision };
 }
+
 module.exports = Flights();
--- logic/passengers.js
+++ logic/passengers.js
@@ -1,4 +1,47 @@
 function Passengers() {
+  function checkFlightCapacity(flightCapacity, passengersArray) {
+    let passengersNumber = 0;
+    let passengers;
+    for (passengers of passengersArray) {
+      passengersNumber += passengers;
+    }
+    if (passengersNumber > flightCapacity) {
+      throw new Error("There are more passengers than the flight capacity.");
+    }
+    return passengersNumber;
+  }
 
+  function distributeAllSeatsToAllPassengers(
+    vipPassengers,
+    regularPassengers,
+    nrOfFlights,
+    businessSeatsPerFlight,
+    economySeatsPerFlight
+  ) {
+
+    let vipPassengersWithBusinessSeats=0, vipPassengersWithEconomySeats=0, 
+        regularPassengersWithBusinessSeats=0, regularPassengersWithEconomySeats=0; //object and its content of variables
+       
+        let availableBusinessSeats = nrOfFlights * businessSeatsPerFlight;
+        let availableEconomySeats = nrOfFlights * economySeatsPerFlight;
+
+        var vipBusinessConfiguration = {passengers:vipPassengers, seats:availableBusinessSeats};
+        vipPassengersWithBusinessSeats = updateConfiguration(vipBusinessConfiguration, businessSeatsPerFlight);
+
+        var vipEconomyConfiguration = {passengers:vipBusinessConfiguration.passengers, seats:availableEconomySeats};
+        vipPassengersWithEconomySeats = updateConfiguration(vipEconomyConfiguration, economySeatsPerFlight);
+
+        var regularBusinessConfiguration = {passengers:regularPassengers, seats:vipBusinessConfiguration.seats};
+        regularPassengersWithBusinessSeats = updateConfiguration(regularBusinessConfiguration, businessSeatsPerFlight);
+
+        var regularEconomyConfiguration = {passengers:regularBusinessConfiguration.passengers, seats:vipEconomyConfiguration.seats};
+        regularPassengersWithEconomySeats = updateConfiguration(regularEconomyConfiguration, economySeatsPerFlight);
+
+        return {vipPassengersWithBusinessSeats:vipPassengersWithBusinessSeats,
+                vipPassengersWithEconomySeats:vipPassengersWithEconomySeats, regularPassengersWithBusinessSeats:regularPassengersWithBusinessSeats,
+                regularPassengersWithEconomySeats:regularPassengersWithEconomySeats};
+    }
+    
 }
 
+module.exports = Passengers();
